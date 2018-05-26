cb1a1fb1b2031165f1f82cfbafba1e2880a07e7e
Juanjo <jherrero@traity.com>
2018-05-26 15:17:28 +0200

    Add request caching to kissanimeac http client

--- src/limbus-provider-kissanimeac/utils/httpClient.ts
+++ src/limbus-provider-kissanimeac/utils/httpClient.ts
@@ -3,11 +3,18 @@ import { KissanimeCloudflareDdosPage } from "../pages";
 import delay from "./delay";
 
 class KissanimeHttpClient {
-  private cache = new Map<string, AxiosResponse>();
+  private cache = new Map<string, Promise<any>>();
   private http = axios.create({ withCredentials: true });
 
   public async request<T = any>(req: AxiosRequestConfig, timesChallenged: number = 0): Promise<T> {
-    return this.http.request<T>(req)
+    const cacheKey = [ JSON.stringify(req), timesChallenged ].join(":");
+    let axiosReq = this.cache.get(cacheKey);
+    if (!axiosReq) {
+      axiosReq = this.http.request<T>(req);
+      this.cache.set(cacheKey, axiosReq);
+    }
+
+    return axiosReq
     .then((res) => res.data)
     .catch((e: AxiosError) => {
       if (
@@ -19,9 +26,11 @@ class KissanimeHttpClient {
         return delay(page.getChallengeDelay())
         .then(() => this.request({ url: page.getChallengeSolutionUrl() }, timesChallenged + 1));
       } else if (timesChallenged >= 5) {
+        this.cache.delete(cacheKey);
         const err = new Error("Cloudflare challenge was not properly resolved after 5 attempts.");
         return Promise.reject(err);
       } else {
+        this.cache.delete(cacheKey);
         return Promise.reject(e);
       }
     });
